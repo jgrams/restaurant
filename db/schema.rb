171d64237b6b0d5a1df590f366f9636d61cbcc48
John Gramila <tynanjk@gmail.com>
2016-03-17 23:43:05 -0500

    Finsihed authetication and validationfor user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318035841) do
+ActiveRecord::Schema.define(version: 20160318041327) do
 
   create_table "owners", force: :cascade do |t|
     t.string   "username"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20160318035841) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
