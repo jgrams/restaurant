aebc2460eaeca980d9fa602ae5be804d2442177e
John Gramila <tynanjk@gmail.com>
2016-03-18 00:53:48 -0500

    added new and show view and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "rests#index"
   
   resources :rests
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
